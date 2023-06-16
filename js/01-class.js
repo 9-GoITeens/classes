f2a162de3a81948854140a23308378c372117137
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-06-16 13:18:06 +0300

    робота на уроці

--- js/01-class.js
+++ js/01-class.js
@@ -31,3 +31,43 @@ const car = new Car({
 });
 
 // ______________________
+
+class Car1 {
+    static description = 'Клас, який описує автомобіль';
+    static logInfo = function (carObj) {
+        console.log('Car.logInfo -> carObj', carObj);
+    };
+
+    constructor({ brand, model, price } = {}) {
+        this.brand = brand;
+        this._model = model;
+        this.price = price;
+    }
+
+    #test = 'test';
+
+    get model() {
+        return this._model;
+    }
+
+    set model(newModel) {
+        this._model = newModel;
+    }
+}
+
+console.dir(Car1);
+
+const carInstense = new Car1({
+    brand: 'Audi',
+    model: 'A6',
+    price: 65000,
+});
+
+console.log(carInstense);
+console.log(carInstense.model);
+console.log((carInstense.model = 'Q4'));
+
+// carInstense.changePrice(1000);
+// Car1.logInfo(carInstense);
+
+// console.log(carInstense.test);
