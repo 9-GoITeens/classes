79d4535645bf1e028c9f8acd03f6998f8b362678
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-06-23 13:21:07 +0300

    робота на уроці

--- js/02-inheritance.js
+++ js/02-inheritance.js
@@ -3,3 +3,49 @@
 //  *  - extends
 //  *  - super()
 //  */
+
+class Hero {
+    constructor({ name, xp }) {
+        this.name = name;
+        this.xp = xp;
+    }
+    gainXp(amaund) {
+        console.log(`${this.name} отримує ${this.xp}`);
+        this.xp += amaund;
+    }
+}
+
+class Warrionr extends Hero {
+    constructor({ weapon, ...resProps } = {}) {
+        super(resProps);
+        this.weapon = weapon;
+    }
+    attack() {
+        console.log(`${this.name} атакує використовуючи ${this.weapon}`);
+    }
+}
+
+const mango = new Warrionr({ name: 'mango', xp: 1000, weapon: 'алебардо' });
+console.log(mango);
+
+mango.attack();
+
+mango.gainXp(2000);
+// console.log(mango);
+
+class Mage extends Hero {
+    constructor({ spells, ...resProps }) {
+        super(resProps);
+        this.spells = spells;
+    }
+
+    cast() {
+        console.log(`${this.name} щось він там кастує `);
+    }
+}
+
+const poly = new Mage({ name: 'poly', xp: 500, spells: ['фаербол'] });
+console.log(poly);
+poly.cast();
+poly.gainXp(200);
+console.log(poly);
--- js/03-practice.js
+++ js/03-practice.js
@@ -3,17 +3,55 @@
 // // age -вік, число
 // // numberOfPosts - кількість постів, число
 // // Клас очікує 1 параметр - обєкт налаштувань з одноіменими властивостями.
+// class User {
+//     constructor({ name, age, numberOfPosts }) {
+//         this.name = name;
+//         this.age = age;
+//         this.numberOfPosts = numberOfPosts;
+//     }
 
-// // Добав метод getInfo(), який повертає строку:
-// // User ${ імя } is ${ вік } years old and has ${ кількість постів } posts.
+//     getInfo() {
+//         return `User ${this.name} is ${this.age} years old and has ${this.numberOfPosts} posts`;
+//     }
+
+//     greeting = () => {
+//         return `Hello ${this.name}`;
+//     };
+// }
+
+// class SuperUser extends User {
+//     constructor({ name, age, numberOfPosts }) {
+//         super({ name, age, numberOfPosts });
+//     }
+// }
+
+// const mango = new User({
+//     name: 'Mango',
+//     age: 24,
+//     numberOfPosts: 20,
+// });
+
+// const bob = new SuperUser({
+//     name: 'Bob',
+//     age: 43,
+//     numberOfPosts: 12,
+// });
+
+// console.log(`mango`, mango);
+
+// console.log(mango.getInfo()); // User Mango is 24 years old and has 20 posts
+// console.log(mango.greeting());
+
+// console.log(`bob`, bob);
+
+// console.log(bob.getInfo()); // User Mango is 24 years old and has 20 posts
+// console.log(bob.greeting());
 
 // const poly = new User({
 //     name: 'Poly',
 //     age: 19,
 //     numberOfPosts: 17,
 // });
-
-// console.log(poly.getInfo()); // User Poly is 19 years old and has 17 posts
 
 // 2) Напиши клас Storage який створює обєкти для управління складом товарів.
 // При виклику буду отримувати один аргумент - початковий масив товарів, і записувати його у властивість items.
@@ -23,6 +61,29 @@
 // getItems() - повертає масив товарів.
 // addItem(item) - отримує новий товар і добавляє його до поточного.
 // removeItem(item) - отримує товар і, якщо він є, удаляє його з поточних.
+
+// class Storage {
+//     constructor(items) {
+//         this.items = items;
+//     }
+
+//     getItems() {
+//         return this.items;
+//     }
+
+//     addItem(item) {
+//         this.items.push(item);
+//     }
+
+//     removeItem(item) {
+//         const index = this.items.indexOf(item);
+//         if (index === -1) {
+//             return;
+//         } else {
+//             this.items.splice(index, 1);
+//         }
+//     }
+// }
 
 // const storage = new Storage(['Яблука', 'Банани', 'Кокоси', 'Ківі']);
 
@@ -36,6 +97,30 @@
 // 3) Напиши клас Client який створює обєкт з властивостями login і email.
 // Оголоси приватні властивості #login і #email, доступ до яких зроби через геттер і сеттер login і email.
 
+// class Client {
+//     #login;
+//     #email;
+//     constructor({ login, email }) {
+//         this.#login = login;
+//         this.#email = email;
+//     }
+
+//     get login() {
+//         return this.#login;
+//     }
+
+//     get email() {
+//         return this.#email;
+//     }
+
+//     set login(newLogin) {
+//         this.#login = newLogin;
+//     }
+//     set email(newEmail) {
+//         this.#email = newEmail;
+//     }
+// }
+
 // const mango = new Client({
 //     login: 'Mango',
 //     email: 'mango@dog.woof',
@@ -46,11 +131,11 @@
 // mango.login = 'Mangodoge';
 // console.log(mango.login); // Mangodoge
 
-//   const poly = new Client({
+// const poly = new Client({
 //     login: 'Poly',
 //     email: 'poly@mail.com',
-//   });
+// });
 
-//   console.log(poly.login); // Poly
-//   poly.login = 'Polycutie';
-//   console.log(poly.login); // Polycutie
+// console.log(poly.login); // Poly
+// poly.login = 'Polycutie';
+// console.log(poly.login); // Polycutie
